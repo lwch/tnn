a190a83d50cfd108ee28a90c31dfa2b6563c3ceb
lwch <lwch748@gmail.com>
2023-04-19 10:27:44 +0800

    Revert "优化reshape接口"
    This reverts commit 518a17bcc1ada26a595a687d0dc27b89bfb7c252.

--- internal/utils/reshape.go
+++ internal/utils/reshape.go
@@ -1,36 +1,27 @@
 package utils
 
-import (
-	"github.com/lwch/tnn/nn/vector"
-	"gonum.org/v1/gonum/mat"
-)
+import "gonum.org/v1/gonum/mat"
 
 func ReshapeRows(input mat.Matrix, rows int) *mat.Dense {
-	// r, c := input.Dims()
-	// data := make([]float64, r*c)
-	// for i := 0; i < r; i++ {
-	// 	for j := 0; j < c; j++ {
-	// 		data[i*c+j] = input.At(i, j)
-	// 	}
-	// }
-	// cols := r * c / rows
-	// return mat.NewDense(rows, cols, data)
 	r, c := input.Dims()
+	data := make([]float64, r*c)
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			data[i*c+j] = input.At(i, j)
+		}
+	}
 	cols := r * c / rows
-	return mat.NewDense(rows, cols, input.(vector.Raw).RawMatrix().Data)
+	return mat.NewDense(rows, cols, data)
 }
 
 func ReshapeCols(input mat.Matrix, cols int) *mat.Dense {
-	// r, c := input.Dims()
-	// data := make([]float64, r*c)
-	// for i := 0; i < r; i++ {
-	// 	for j := 0; j < c; j++ {
-	// 		data[i*c+j] = input.At(i, j)
-	// 	}
-	// }
-	// rows := r * c / cols
-	// return mat.NewDense(rows, cols, data)
 	r, c := input.Dims()
+	data := make([]float64, r*c)
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			data[i*c+j] = input.At(i, j)
+		}
+	}
 	rows := r * c / cols
-	return mat.NewDense(rows, cols, input.(vector.Raw).RawMatrix().Data)
+	return mat.NewDense(rows, cols, data)
 }
