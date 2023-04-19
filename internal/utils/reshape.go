518a17bcc1ada26a595a687d0dc27b89bfb7c252
lwch <lwch748@gmail.com>
2023-04-19 10:23:58 +0800

    优化reshape接口

--- internal/utils/reshape.go
+++ internal/utils/reshape.go
@@ -1,27 +1,36 @@
 package utils
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"github.com/lwch/tnn/nn/vector"
+	"gonum.org/v1/gonum/mat"
+)
 
 func ReshapeRows(input mat.Matrix, rows int) *mat.Dense {
+	// r, c := input.Dims()
+	// data := make([]float64, r*c)
+	// for i := 0; i < r; i++ {
+	// 	for j := 0; j < c; j++ {
+	// 		data[i*c+j] = input.At(i, j)
+	// 	}
+	// }
+	// cols := r * c / rows
+	// return mat.NewDense(rows, cols, data)
 	r, c := input.Dims()
-	data := make([]float64, r*c)
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			data[i*c+j] = input.At(i, j)
-		}
-	}
 	cols := r * c / rows
-	return mat.NewDense(rows, cols, data)
+	return mat.NewDense(rows, cols, input.(vector.Raw).RawMatrix().Data)
 }
 
 func ReshapeCols(input mat.Matrix, cols int) *mat.Dense {
+	// r, c := input.Dims()
+	// data := make([]float64, r*c)
+	// for i := 0; i < r; i++ {
+	// 	for j := 0; j < c; j++ {
+	// 		data[i*c+j] = input.At(i, j)
+	// 	}
+	// }
+	// rows := r * c / cols
+	// return mat.NewDense(rows, cols, data)
 	r, c := input.Dims()
-	data := make([]float64, r*c)
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			data[i*c+j] = input.At(i, j)
-		}
-	}
 	rows := r * c / cols
-	return mat.NewDense(rows, cols, data)
+	return mat.NewDense(rows, cols, input.(vector.Raw).RawMatrix().Data)
 }
