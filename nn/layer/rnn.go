cefe1d092aab5e37033f02837d981ff9d21cbd84
lwch <lwch748@gmail.com>
2023-05-23 21:26:44 +0800

    修改rnn层变量命名

--- nn/layer/rnn.go
+++ nn/layer/rnn.go
@@ -14,10 +14,10 @@ type Rnn struct {
 func NewRnn(featureSize, times, hidden int, init initializer.Initializer) Layer {
 	var layer Rnn
 	layer.base = new("rnn", map[string]Shape{
-		"dw": {featureSize, hidden},
-		"db": {1, hidden},
-		"hw": {hidden, hidden},
-		"hb": {1, hidden},
+		"Wih": {featureSize, hidden},
+		"Bih": {1, hidden},
+		"Whh": {hidden, hidden},
+		"Bhh": {1, hidden},
 	}, init)
 	layer.featureSize = featureSize
 	layer.times = times
@@ -25,21 +25,22 @@ func NewRnn(featureSize, times, hidden int, init initializer.Initializer) Layer 
 	return &layer
 }
 
+// Forward https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
 func (layer *Rnn) Forward(input *tensor.Tensor, isTraining bool) *tensor.Tensor {
 	if !layer.hasInit {
 		layer.initParams()
 	}
-	dw := layer.params.Get("dw")
-	db := layer.params.Get("db")
-	hw := layer.params.Get("hw")
-	hb := layer.params.Get("hb")
+	Wih := layer.params.Get("Wih")
+	Bih := layer.params.Get("Bih")
+	Whh := layer.params.Get("Whh")
+	Bhh := layer.params.Get("Bhh")
 	batchSize, _ := input.Dims()
 	state := tensor.New(nil, batchSize, layer.hidden)
 	for t := layer.times - 1; t >= 0; t-- {
 		start := t * layer.featureSize
 		t := input.Slice(0, batchSize, start, start+layer.featureSize)
-		l1 := t.Mul(dw).AddVector(db)
-		l2 := state.Mul(hw).AddVector(hb)
+		l1 := t.Mul(Wih).AddVector(Bih)
+		l2 := state.Mul(Whh).AddVector(Bhh)
 		state = l1.Add(l2).Tanh()
 	}
 	return state
