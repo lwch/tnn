d500d92f0f81f4b6ffacce3422daa44cc9425fe3
lwch <lwch748@gmail.com>
2023-05-27 23:47:05 +0800

    去除pad

--- example/couplet/data.go
+++ example/couplet/data.go
@@ -95,9 +95,6 @@ func loadData(dir string, idx map[string]int) [][]int {
 			row = append(row, idx[v])
 		}
 		row = append(row, 1) // </s>
-		for len(row) < paddingSize {
-			row = append(row, 1)
-		}
 		data = append(data, row)
 		if len(row) > max {
 			max = len(row)
