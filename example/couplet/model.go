22a7e9d111093b85fe675191e6b48ff0ffe00b23
lwch <lwch748@gmail.com>
2023-05-28 01:15:40 +0800

    修改loss函数

--- example/couplet/model.go
+++ example/couplet/model.go
@@ -57,8 +57,8 @@ func loadEmbedding(vocabSize int) [][]float64 {
 
 func train(trainX, trainY [][]int, embedding [][]float64) {
 	initModel(len(embedding))
-	// loss := loss.NewSoftmax()
-	loss := loss.NewMSE()
+	loss := loss.NewSoftmax()
+	// loss := loss.NewMSE()
 	optimizer := optimizer.NewAdam(lr, 0, 0.9, 0.999, 1e-8)
 
 	ch := make(chan []int)
