7f45ded7b44359d4811c4cd06f43f3155e8c44f8
lwch <lwch748@gmail.com>
2023-05-30 18:42:23 +0800

    修改nor层实现方式

--- nn/layer/nor_test.go
+++ nn/layer/nor_test.go
@@ -9,11 +9,11 @@ import (
 )
 
 func TestNor(t *testing.T) {
-	x := tensor.New([]float64{1, 2, 3, 4, 5, 6}, 2, 3)
+	x := tensor.New([]float64{1, 3, 5, 2, 4, 8}, 2, 3)
 	x.SetRequireGrad(true)
 	layer := NewNor()
 	y := layer.Forward(x, false)
 	fmt.Println(mat.Formatted(y.Value()))
-	y.Backward(tensor.New([]float64{0.1, 0.2, 0.3, 0.4, 0.5, 0.6}, 2, 3))
+	y.Backward(tensor.New([]float64{0.1, 0.3, 0.5, 0.2, 0.4, 0.6}, 2, 3))
 	fmt.Println(mat.Formatted(x.Grad().Value()))
 }
