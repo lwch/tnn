04785b6c03e7a8df53f25cc1250e7d8bf76b1228
lwch <lwch748@gmail.com>
2023-05-31 00:25:28 +0800

    修改模型参数

--- example/couplet/model.go
+++ example/couplet/model.go
@@ -33,7 +33,7 @@ const unitSize = paddingSize * embeddingDim * 2
 const head = 2
 const batchSize = 16
 const epoch = 1000
-const lr = 0.01
+const lr = 0.001
 const transformerSize = 1
 
 func buildEmbedding(vocabSize int) {
