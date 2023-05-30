30d30a409c989569e593bab3514cb4bca4eed774
lwch <lwch748@gmail.com>
2023-05-30 18:05:31 +0800

    增加log

--- example/couplet/model.go
+++ example/couplet/model.go
@@ -24,6 +24,7 @@ import (
 	"github.com/lwch/tnn/nn/optimizer"
 	"github.com/lwch/tnn/nn/params"
 	"github.com/lwch/tnn/nn/tensor"
+	"gonum.org/v1/gonum/mat"
 )
 
 const modelDir = "./model"
@@ -284,9 +285,10 @@ func forwardTransformer(i int, x, y *tensor.Tensor, train bool) (*tensor.Tensor,
 	// 	y = dropout.Forward(y, true)
 	// }
 	selfOut := layers[i+1].Forward(y, train) // nor
-	y = layers[i+2].Forward(selfOut, train)  // dense
-	y = layers[i+3].Forward(y, train)        // relu
-	y = layers[i+4].Forward(y, train)        // dense
+	fmt.Println(mat.Formatted(selfOut.Value()))
+	y = layers[i+2].Forward(selfOut, train) // dense
+	y = layers[i+3].Forward(y, train)       // relu
+	y = layers[i+4].Forward(y, train)       // dense
 	y = y.Add(selfOut)
 	// if train {
 	// 	y = dropout.Forward(y, true)
