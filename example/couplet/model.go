436d28f918734691950e09f7fef9eb67dbc94904
lwch <lwch748@gmail.com>
2023-05-30 16:50:55 +0800

    增加log

--- example/couplet/model.go
+++ example/couplet/model.go
@@ -24,6 +24,7 @@ import (
 	"github.com/lwch/tnn/nn/optimizer"
 	"github.com/lwch/tnn/nn/params"
 	"github.com/lwch/tnn/nn/tensor"
+	"gonum.org/v1/gonum/mat"
 )
 
 const modelDir = "./model"
@@ -131,6 +132,9 @@ func trainWorker(loss loss.Loss, trainX, trainY [][]int,
 			xOut = append(xOut, dup(trainY[i]))
 		}
 		x, y, z := buildTensor(xIn, xOut, vocabs, embedding, true)
+		fmt.Println(mat.Formatted(x.Value()))
+		fmt.Println(mat.Formatted(y.Value()))
+		fmt.Println(mat.Formatted(z.Value()))
 		pred := forward(x, y, true)
 		grad := loss.Loss(pred, z)
 		grad.Backward(grad)
