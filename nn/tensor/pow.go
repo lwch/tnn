158624f8d0efbf3d9156a434161d04a5639dda33
lwch <lwch748@gmail.com>
2023-05-30 10:42:10 +0800

    优化pow算子的反向传播速度

--- nn/tensor/pow.go
+++ nn/tensor/pow.go
@@ -1,23 +1,33 @@
 package tensor
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
 
 type pow struct {
-	a *Tensor
-	b float64
+	a         *Tensor
+	b         float64
+	value     mat.Dense
+	gradValue mat.Dense
 }
 
 func (op *pow) f() *mat.Dense {
-	value := powDense(op.a.Value(), op.b)
-	return value
+	op.value.Apply(func(i, j int, v float64) float64 {
+		return math.Pow(v, op.b)
+	}, op.a.Value())
+	op.gradValue.Apply(func(i, j int, v float64) float64 {
+		return op.b * math.Pow(v, op.b-1)
+	}, op.a.Value())
+	return &op.value
 }
 
 func (op *pow) df(grad *Tensor) {
-	pow := powDense(op.a.Value(), op.b-1)
-	pow.Scale(op.b, pow)
-	delta := grad.MulElem(FromDense(pow))
-	op.a.AddGrad(delta.Value())
-	op.a.Backward(delta)
+	var delta mat.Dense
+	delta.MulElem(grad.Value(), &op.gradValue)
+	op.a.AddGrad(&delta)
+	op.a.Backward(FromDense(&delta))
 }
 
 func (op *pow) ZeroGrad() {
