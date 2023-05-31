abf2e427869d82ebcc983541d4ca5536c8184a84
lwch <lwch748@gmail.com>
2023-05-31 18:31:09 +0800

    修改输出内容

--- example/couplet/predict.go
+++ example/couplet/predict.go
@@ -29,7 +29,7 @@ func predict(str string, vocabs []string, vocab2idx map[string]int, embedding []
 	dy := make([]int, 0, len(str))
 	for i := 0; i < size; i++ {
 		// x, y, _ := buildTensor([][]int{dx}, [][]int{dy}, vocabs, embedding, false)
-		fmt.Println(dx, dy)
+		// fmt.Println(dx, dy)
 		x, _, paddingMask := build(dx, dy, 0, vocabs, embedding)
 		pred := forward(tensor.New(x, 1, unitSize), buildPaddingMasks([][]bool{paddingMask}), false)
 		predProb := pred.Value().RowView(0).(*mat.VecDense).RawVector().Data
