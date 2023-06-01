4a088015de870091b328967cb2926713d2ef4355
lwch <lwch748@gmail.com>
2023-06-01 20:31:49 +0800

    修改embedding size

--- example/couplet/logic/model/params.go
+++ example/couplet/logic/model/params.go
@@ -1,7 +1,7 @@
 package model
 
-const embeddingDim = 128 // 2个float64表示一个字向量
-const paddingSize = 74   // 最长为34*2，因此padding长度必须大于68+2
+const embeddingDim = 32 // 2个float64表示一个字向量
+const paddingSize = 74  // 最长为34*2，因此padding长度必须大于68+2
 const unitSize = paddingSize * embeddingDim
 const head = 8
 const batchSize = 8
