fbc7d44b427e3785085bd67a4f31b96297e8e02e
lwch <lwch748@gmail.com>
2023-06-01 20:25:35 +0800

    修改模型参数

--- example/couplet/logic/model/params.go
+++ example/couplet/logic/model/params.go
@@ -3,8 +3,8 @@ package model
 const embeddingDim = 2 // 2个float64表示一个字向量
 const paddingSize = 74 // 最长为34*2，因此padding长度必须大于68+2
 const unitSize = paddingSize * embeddingDim
-const head = 1
+const head = 8
 const batchSize = 8
 const epoch = 10000
 const lr = 0.0001
-const transformerSize = 8
+const transformerSize = 16
