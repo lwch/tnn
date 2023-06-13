cad703707b8e07375befae94db47cefad610baa8
lwch <lwch748@gmail.com>
2023-06-13 18:39:07 +0800

    实现evaluate

--- example/couplet/logic/model/evaluate.go
+++ example/couplet/logic/model/evaluate.go
@@ -3,30 +3,31 @@ package model
 import (
 	"fmt"
 	"sort"
+
+	"github.com/lwch/gotorch/tensor"
+	"github.com/lwch/tnn/example/couplet/logic/sample"
 )
 
 // Evaluate 根据输入内容进行推理
 func (m *Model) Evaluate(str string) string {
-	// dx := make([]int, 0, len(str))
-	// var size int
-	// for _, ch := range str {
-	// 	dx = append(dx, m.vocabsIdx[string(ch)])
-	// 	size++
-	// }
-	// dx = append(dx, 1) // </s>
-	// dy := make([]int, 0, len(str))
-	// dy = append(dy, 0) // <s>
-	// for i := 0; i < size; i++ {
-	// 	x, _, paddingMask := sample.Build(append(dx, dy...), 0, paddingSize, m.embedding, m.vocabs)
-	// 	pred := m.forward(tensor.New(x, 1, unitSize), buildPaddingMasks([][]bool{paddingMask}), false)
-	// 	predProb := pred.Value().RowView(0).(*mat32.VecDense).RawVector().Data
-	// 	label := lookup(predProb, m.vocabs)
-	// 	dy = append(dy, label)
-	// }
-	// return values(m.vocabs, dy[1:])
-
-	// TODO
-	return ""
+	dx := make([]int, 0, len(str))
+	var size int
+	for _, ch := range str {
+		dx = append(dx, m.vocabsIdx[string(ch)])
+		size++
+	}
+	dx = append(dx, 1) // </s>
+	dy := make([]int, 0, len(str))
+	dy = append(dy, 0) // <s>
+	for i := 0; i < size; i++ {
+		x, _, _ := sample.Build(append(dx, dy...), 0, paddingSize, m.embedding, m.vocabs)
+		// pred := m.forward(tensor.New(x, 1, unitSize), buildPaddingMasks([][]bool{paddingMask}), false)
+		pred := m.forward(tensor.FromFloat32(nil, x, 1, paddingSize, embeddingDim), nil, false)
+		predProb := pred.Float32Value()
+		label := lookup(predProb, m.vocabs)
+		dy = append(dy, label)
+	}
+	return values(m.vocabs, dy[1:])
 }
 
 func lookup(prob []float32, vocabs []string) int {
