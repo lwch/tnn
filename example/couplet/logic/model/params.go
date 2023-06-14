011f29541649db465c695dd9e25bf8762c0c9b67
lwch <lwch748@gmail.com>
2023-06-14 14:43:27 +0800

    增加注释

--- example/couplet/logic/model/params.go
+++ example/couplet/logic/model/params.go
@@ -1,5 +1,19 @@
 package model
 
+// +------------------------+---------+
+// |          NAME          |  COUNT  |
+// +------------------------+---------+
+// | transformer0_attention |    1968 |
+// | transformer0_dense     | 1404224 |
+// | transformer0_output    | 1402448 |
+// | transformer1_attention |    1968 |
+// | transformer1_dense     | 1404224 |
+// | transformer1_output    | 1402448 |
+// | output                 | 2630548 |
+// | total                  | 8247828 |
+// +------------------------+---------+
+// 以下参数配置总计824万个参数
+
 const embeddingDim = 8 // 8个float32表示一个字向量
 const paddingSize = 74 // 最长为34*2，因此padding长度必须大于68+2
 const heads = 4
