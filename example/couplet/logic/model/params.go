00fad97543ae5647abfb72d7be038aabf1b881e7
lwch <lwch748@gmail.com>
2023-06-17 21:37:41 +0800

    修改参数

--- example/couplet/logic/model/params.go
+++ example/couplet/logic/model/params.go
@@ -5,6 +5,6 @@ const paddingSize = 34   // 最长为34
 const heads = 8
 const maskSize = paddingSize * paddingSize
 const batchSize = 128
-const epoch = 1000
+const epoch = 200
 const lr = 0.001
 const transformerSize = 4
