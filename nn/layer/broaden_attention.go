f667590a7142d2aec57bf9e13af9ebc6bd4119f0
lwch <lwch748@gmail.com>
2023-06-21 11:18:35 +0800

    增加broaden_attention层

--- nn/layer/broaden_attention.go
+++ nn/layer/broaden_attention.go
@@ -8,21 +8,22 @@ import (
 	"github.com/lwch/tnn/internal/pb"
 )
 
-type SelfAttention struct {
+type BroadenAttention struct {
 	*base
-	steps, dims, heads int
-	dropout            float64
-	scale              *tensor.Tensor
+	steps, dims, multiple, heads int
+	dropout                      float64
+	scale                        *tensor.Tensor
 	// params
 	wq, wk, wv *tensor.Tensor
 	bq, bk, bv *tensor.Tensor
 }
 
-func NewSelfAttention(steps, dims, heads int, dropout float64, device consts.DeviceType) *SelfAttention {
-	var layer SelfAttention
-	layer.base = new("self_attention", device)
+func NewBroadenAttention(steps, dims, multiple, heads int, dropout float64, device consts.DeviceType) *BroadenAttention {
+	var layer BroadenAttention
+	layer.base = new("broaden_attention", device)
 	layer.steps = steps
 	layer.dims = dims
+	layer.multiple = multiple
 	layer.heads = heads
 	layer.dropout = dropout
 	if layer.dims%layer.heads != 0 {
@@ -31,12 +32,13 @@ func NewSelfAttention(steps, dims, heads int, dropout float64, device consts.Dev
 	return &layer
 }
 
-func LoadSelfAttention(device consts.DeviceType, name string, params map[string]*pb.Dense, args map[string]float32) Layer {
-	var layer SelfAttention
+func LoadBroadenAttention(device consts.DeviceType, name string, params map[string]*pb.Dense, args map[string]float32) Layer {
+	var layer BroadenAttention
 	layer.base = new("self_attention", device)
 	layer.name = name
 	layer.steps = int(args["steps"])
 	layer.dims = int(args["dims"])
+	layer.multiple = int(args["multiple"])
 	layer.heads = int(args["heads"])
 	layer.dropout = float64(args["dropout"])
 	layer.wq = layer.loadParam(params["Wq"])
@@ -48,34 +50,34 @@ func LoadSelfAttention(device consts.DeviceType, name string, params map[string]
 	return &layer
 }
 
-func (layer *SelfAttention) Forward(q, k, mask *tensor.Tensor, train bool) *tensor.Tensor {
+func (layer *BroadenAttention) Forward(q, k, mask *tensor.Tensor, train bool) *tensor.Tensor {
 	if layer.scale == nil {
 		layer.scale = tensor.FromFloat32(nil, []float32{float32(math.Sqrt(float64(layer.dims)))}, tensor.WithShapes(1))
 	}
 	if layer.wq == nil {
-		layer.wq = layer.initW(int64(layer.dims), int64(layer.dims))
+		layer.wq = layer.initW(int64(layer.dims), int64(layer.dims*layer.multiple))
 	}
 	if layer.wk == nil {
-		layer.wk = layer.initW(int64(layer.dims), int64(layer.dims))
+		layer.wk = layer.initW(int64(layer.dims), int64(layer.dims*layer.multiple))
 	}
 	if layer.wv == nil {
 		layer.wv = layer.initW(int64(layer.dims), int64(layer.dims))
 	}
 	if layer.bq == nil {
-		layer.bq = layer.initB(int64(layer.steps), int64(layer.dims))
+		layer.bq = layer.initB(int64(layer.steps), int64(layer.dims*layer.multiple))
 	}
 	if layer.bk == nil {
-		layer.bk = layer.initB(int64(layer.steps), int64(layer.dims))
+		layer.bk = layer.initB(int64(layer.steps), int64(layer.dims*layer.multiple))
 	}
 	if layer.bv == nil {
 		layer.bv = layer.initB(int64(layer.steps), int64(layer.dims))
 	}
-	q = q.MatMul(layer.wq).Add(layer.bq)  // (batch, steps, dims)
+	q = q.MatMul(layer.wq).Add(layer.bq)  // (batch, steps, dims*multiple)
 	v := k.MatMul(layer.wv).Add(layer.bv) // (batch, steps, dims)
-	k = k.MatMul(layer.wk).Add(layer.bk)  // (batch, steps, dims)
-	q = layer.split(q)                    // (batch, heads, steps, dims/heads)
-	k = layer.split(k)                    // (batch, heads, steps, dims/heads)
-	v = layer.split(v)                    // (batch, heads, steps, dims/heads)
+	k = k.MatMul(layer.wk).Add(layer.bk)  // (batch, steps, dims*multiple)
+	q = layer.split(q, layer.multiple)    // (batch, heads, steps, dims/heads*multiple)
+	k = layer.split(k, layer.multiple)    // (batch, heads, steps, dims/heads*multiple)
+	v = layer.split(v, 1)                 // (batch, heads, steps, dims/heads)
 	y := q.MatMul(k.Transpose(-1, -2))    // (batch, heads, steps, steps)
 	y = y.Div(layer.scale)                // (batch, heads, steps, steps)
 	if mask != nil {
@@ -89,20 +91,20 @@ func (layer *SelfAttention) Forward(q, k, mask *tensor.Tensor, train bool) *tens
 	return y
 }
 
-func (layer *SelfAttention) split(x *tensor.Tensor) *tensor.Tensor {
+func (layer *BroadenAttention) split(x *tensor.Tensor, multiple int) *tensor.Tensor {
 	inputShape := x.Shapes()
-	v := x.View(inputShape[0], int64(layer.steps), int64(layer.heads), int64(layer.dims/layer.heads))
+	v := x.View(inputShape[0], int64(layer.steps), int64(layer.heads), int64(layer.dims/layer.heads*multiple))
 	return v.Permute(0, 2, 1, 3)
 }
 
-func (layer *SelfAttention) Params() map[string]*tensor.Tensor {
+func (layer *BroadenAttention) Params() map[string]*tensor.Tensor {
 	return map[string]*tensor.Tensor{
 		"Wq": layer.wq, "Wk": layer.wk, "Wv": layer.wv,
 		"Bq": layer.bq, "Bk": layer.bk, "Bv": layer.bv,
 	}
 }
 
-func (layer *SelfAttention) Args() map[string]float32 {
+func (layer *BroadenAttention) Args() map[string]float32 {
 	return map[string]float32{
 		"steps":   float32(layer.steps),
 		"dims":    float32(layer.dims),
