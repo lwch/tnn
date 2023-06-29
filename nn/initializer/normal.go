e98bdd2456b8f8f97c1a785c42b8d3f59bff016c
lwch <lwch748@gmail.com>
2023-06-29 11:41:40 +0800

    增加高斯initializer

---
+++ nn/initializer/normal.go
@@ -0,0 +1,40 @@
+package initializer
+
+import "gonum.org/v1/gonum/stat/distuv"
+
+type Normal struct {
+	n distuv.Normal
+}
+
+func NewNormal(mean, stddev float64) *Normal {
+	return &Normal{
+		n: distuv.Normal{
+			Mu:    mean,
+			Sigma: stddev,
+		},
+	}
+}
+
+func (rand *Normal) Rand() float64 {
+	return rand.n.Rand()
+}
+
+func (rand *Normal) RandN(n int) []float64 {
+	ret := make([]float64, n)
+	for i := 0; i < n; i++ {
+		ret[i] = rand.n.Rand()
+	}
+	return ret
+}
+
+func (rand *Normal) RandShape(shapes ...int64) []float64 {
+	size := int64(1)
+	for _, s := range shapes {
+		size *= s
+	}
+	ret := make([]float64, size)
+	for i := 0; i < int(size); i++ {
+		ret[i] = rand.n.Rand()
+	}
+	return ret
+}
