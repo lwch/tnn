55e08746a05c011bd80674bb2156dfa85d5455c3
lwch <lwch748@gmail.com>
2023-07-06 17:47:08 +0800

    去除layer_norm层中的训练参数

--- nn/layer/layer_norm.go
+++ nn/layer/layer_norm.go
@@ -9,9 +9,6 @@ import (
 type LayerNorm struct {
 	*base
 	eps *tensor.Tensor
-	// params
-	w *tensor.Tensor
-	b *tensor.Tensor
 }
 
 func NewLayerNorm(device consts.DeviceType) *LayerNorm {
@@ -24,8 +21,6 @@ func LoadLayerNorm(device consts.DeviceType, name string, params map[string]*pb.
 	var layer LayerNorm
 	layer.base = new("layer_norm", device)
 	layer.name = name
-	layer.w = layer.loadParam(params["w"])
-	layer.b = layer.loadParam(params["b"])
 	return &layer
 }
 
@@ -33,38 +28,7 @@ func (layer *LayerNorm) Forward(x *tensor.Tensor) *tensor.Tensor {
 	if layer.eps == nil {
 		layer.eps = tensor.FromFloat32(nil, []float32{1e-9}, tensor.WithShapes(1), tensor.WithDevice(layer.device))
 	}
-	if layer.w == nil {
-		layer.w = layer.initW(lastDim(x))
-	}
-	if layer.b == nil {
-		layer.b = layer.initB(lastDim(x))
-	}
 	mean := x.Mean(-1, true)
 	v := x.Var(-1, false, true)
-	return layer.w.Mul(x.Sub(mean)).Div(v.Add(layer.eps).Sqrt()).Add(layer.b)
-}
-
-func (layer *LayerNorm) Params() map[string]*tensor.Tensor {
-	return map[string]*tensor.Tensor{
-		"w": layer.w,
-		"b": layer.b,
-	}
-}
-
-func (layer *LayerNorm) Freeze() {
-	if layer.w != nil {
-		layer.w.SetRequiresGrad(false)
-	}
-	if layer.b != nil {
-		layer.b.SetRequiresGrad(false)
-	}
-}
-
-func (layer *LayerNorm) Unfreeze() {
-	if layer.w != nil {
-		layer.w.SetRequiresGrad(true)
-	}
-	if layer.b != nil {
-		layer.b.SetRequiresGrad(true)
-	}
+	return x.Sub(mean).Div(v.Add(layer.eps).Sqrt())
 }
