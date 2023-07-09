fe484295286e8f34727c1481c988b5f1343e37da
lwch <lwch748@gmail.com>
2023-07-09 22:14:12 +0800

    去除conv_1d层中的bias

--- nn/layer/conv_1d.go
+++ nn/layer/conv_1d.go
@@ -16,7 +16,6 @@ type Conv1D struct {
 	groups    int
 	// params
 	w *tensor.Tensor
-	b *tensor.Tensor
 }
 
 func NewConv1D(inC, outC, kernel int, device consts.DeviceType) *Conv1D {
@@ -60,7 +59,6 @@ func LoadConv1D(device consts.DeviceType, name string, params map[string]*pb.Den
 	layer.dilation = int(args["dilation"])
 	layer.groups = int(args["groups"])
 	layer.w = layer.loadParam(params["w"])
-	layer.b = layer.loadParam(params["b"])
 	return &layer
 }
 
@@ -68,10 +66,7 @@ func (layer *Conv1D) Forward(x *tensor.Tensor) *tensor.Tensor {
 	if layer.w == nil {
 		layer.w = layer.initW(int64(layer.outC), int64(layer.inC/layer.groups), int64(layer.kernel))
 	}
-	if layer.b == nil {
-		layer.b = layer.initB(int64(layer.outC))
-	}
-	return x.Conv1D(layer.w, layer.b,
+	return x.Conv1D(layer.w, nil,
 		tensor.ConvStride(layer.stride),
 		tensor.ConvPadding(layer.padding),
 		tensor.ConvDilation(layer.dilation),
@@ -81,7 +76,6 @@ func (layer *Conv1D) Forward(x *tensor.Tensor) *tensor.Tensor {
 func (layer *Conv1D) Params() map[string]*tensor.Tensor {
 	return map[string]*tensor.Tensor{
 		"w": layer.w,
-		"b": layer.b,
 	}
 }
 
@@ -101,16 +95,10 @@ func (layer *Conv1D) Freeze() {
 	if layer.w != nil {
 		layer.w.SetRequiresGrad(false)
 	}
-	if layer.b != nil {
-		layer.b.SetRequiresGrad(false)
-	}
 }
 
 func (layer *Conv1D) Unfreeze() {
 	if layer.w != nil {
 		layer.w.SetRequiresGrad(true)
-	}
-	if layer.b != nil {
-		layer.b.SetRequiresGrad(true)
 	}
 }
