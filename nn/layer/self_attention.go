dbfde5c7501761788a969601d3e59001a4b95acf
lwch <lwch748@gmail.com>
2023-07-09 21:38:11 +0800

    去除attention层的bias参数

--- nn/layer/self_attention.go
+++ nn/layer/self_attention.go
@@ -13,7 +13,6 @@ type SelfAttention struct {
 	isCausal      bool
 	// params
 	wq, wk, wv *tensor.Tensor
-	bq, bk, bv *tensor.Tensor
 }
 
 func NewSelfAttention(hidden, heads int, dropout float64, isCausal bool, device consts.DeviceType) *SelfAttention {
@@ -40,9 +39,6 @@ func LoadSelfAttention(device consts.DeviceType, name string, params map[string]
 	layer.wq = layer.loadParam(params["Wq"])
 	layer.wk = layer.loadParam(params["Wk"])
 	layer.wv = layer.loadParam(params["Wv"])
-	layer.bq = layer.loadParam(params["Bq"])
-	layer.bk = layer.loadParam(params["Bk"])
-	layer.bv = layer.loadParam(params["Bv"])
 	return &layer
 }
 
@@ -67,21 +63,12 @@ func (layer *SelfAttention) Forward(q, k, v, mask *tensor.Tensor, train bool) *t
 	if layer.wv == nil {
 		layer.wv = layer.initW(lastDim(v), int64(layer.hidden))
 	}
-	if layer.bq == nil {
-		layer.bq = layer.initB(int64(layer.hidden))
-	}
-	if layer.bk == nil {
-		layer.bk = layer.initB(int64(layer.hidden))
-	}
-	if layer.bv == nil {
-		layer.bv = layer.initB(int64(layer.hidden))
-	}
-	q = q.MatMul(layer.wq).Add(layer.bq) // (batch, steps, hidden)
-	k = k.MatMul(layer.wk).Add(layer.bk) // (batch, steps, hidden)
-	v = v.MatMul(layer.wv).Add(layer.bv) // (batch, steps, hidden)
-	q = layer.split(q)                   // (batch, heads, steps, hidden/heads)
-	k = layer.split(k)                   // (batch, heads, steps, hidden/heads)
-	v = layer.split(v)                   // (batch, heads, steps, hidden/heads)
+	q = q.MatMul(layer.wq) // (batch, steps, hidden)
+	k = k.MatMul(layer.wk) // (batch, steps, hidden)
+	v = v.MatMul(layer.wv) // (batch, steps, hidden)
+	q = layer.split(q)     // (batch, heads, steps, hidden/heads)
+	k = layer.split(k)     // (batch, heads, steps, hidden/heads)
+	v = layer.split(v)     // (batch, heads, steps, hidden/heads)
 	dropout := layer.dropout
 	if !train {
 		dropout = 0
@@ -101,7 +88,6 @@ func (layer *SelfAttention) split(x *tensor.Tensor) *tensor.Tensor {
 func (layer *SelfAttention) Params() map[string]*tensor.Tensor {
 	return map[string]*tensor.Tensor{
 		"Wq": layer.wq, "Wk": layer.wk, "Wv": layer.wv,
-		"Bq": layer.bq, "Bk": layer.bk, "Bv": layer.bv,
 	}
 }
 
@@ -128,15 +114,6 @@ func (layer *SelfAttention) Freeze() {
 	if layer.wv != nil {
 		layer.wv.SetRequiresGrad(false)
 	}
-	if layer.bq != nil {
-		layer.bq.SetRequiresGrad(false)
-	}
-	if layer.bk != nil {
-		layer.bk.SetRequiresGrad(false)
-	}
-	if layer.bv != nil {
-		layer.bv.SetRequiresGrad(false)
-	}
 }
 
 func (layer *SelfAttention) Unfreeze() {
@@ -148,14 +125,5 @@ func (layer *SelfAttention) Unfreeze() {
 	}
 	if layer.wv != nil {
 		layer.wv.SetRequiresGrad(true)
-	}
-	if layer.bq != nil {
-		layer.bq.SetRequiresGrad(true)
-	}
-	if layer.bk != nil {
-		layer.bk.SetRequiresGrad(true)
-	}
-	if layer.bv != nil {
-		layer.bv.SetRequiresGrad(true)
 	}
 }
