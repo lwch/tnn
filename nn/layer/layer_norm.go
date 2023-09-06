29590bf14ce4b6a8b2b9af74f73300e6f7230406
lwch <lwch748@gmail.com>
2023-09-06 14:09:34 +0800

    修改layer_norm层，增加训练参数

--- nn/layer/layer_norm.go
+++ nn/layer/layer_norm.go
@@ -9,11 +9,26 @@ import (
 type LayerNorm struct {
 	base
 	eps *tensor.Tensor
+	// params
+	a *tensor.Tensor
+	b *tensor.Tensor
 }
 
-func NewLayerNorm(opts ...LayerCreateOption) *LayerNorm {
+func NewLayerNorm(dims int64, opts ...LayerCreateOption) *LayerNorm {
 	var layer LayerNorm
 	layer.new("layer_norm", opts...)
+	data := make([]float32, dims)
+	for i := range data {
+		data[i] = 1
+	}
+	layer.eps = tensor.FromFloat32(nil, []float32{1e-9},
+		tensor.WithShapes(1),
+		tensor.WithDevice(layer.device))
+	layer.a = tensor.FromFloat32(nil, data,
+		tensor.WithShapes(dims),
+		tensor.WithDevice(layer.device))
+	layer.a.SetRequiresGrad(true)
+	layer.b = layer.initB(dims)
 	return &layer
 }
 
@@ -21,16 +36,19 @@ func LoadLayerNorm(device consts.DeviceType, name string, params map[string]*pb.
 	var layer LayerNorm
 	layer.new("layer_norm", WithDevice(device))
 	layer.name = name
+	layer.eps = tensor.FromFloat32(nil, []float32{1e-9},
+		tensor.WithShapes(1),
+		tensor.WithDevice(layer.device))
+	layer.a = layer.loadParam(params["a"])
+	layer.b = layer.loadParam(params["b"])
 	return &layer
 }
 
 func (layer *LayerNorm) Forward(x *tensor.Tensor) *tensor.Tensor {
-	if layer.eps == nil {
-		layer.eps = tensor.FromFloat32(nil, []float32{1e-9}, tensor.WithShapes(1), tensor.WithDevice(layer.device))
-	}
 	mean := x.Mean(-1, true)
 	v := x.Var(-1, false, true)
 	sub := x.Sub(mean)
 	bias := v.Add(layer.eps)
-	return sub.Div(bias)
+	div := sub.Div(bias)
+	return div.Mul(layer.a).Add(layer.b)
 }
